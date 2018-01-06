707b0285c38b2aa6bae3047cb9d395d6bfca8308
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2018-01-07 01:01:23 +0700

    update source info

--- views/o/history_pause/front_view.php
+++ views/o/history_pause/front_view.php
@@ -3,12 +3,11 @@
  * Kanban Task History Pauses (kanban-task-history-pause)
  * @var $this HistorypauseController 
  * @var $model KanbanTaskHistoryPause
- * version: 0.0.1
  *
  * @author Putra Sudaryanto <putra@sudaryanto.id>
+ * @contact (+62)856-299-4114
  * @copyright Copyright (c) 2013 Ommu Platform (opensource.ommu.co)
  * @link https://github.com/ommu/ommu-kanban-task
- * @contact (+62)856-299-4114
  *
  */
 
--- views/o/user/admin_manage.php
+++ views/o/user/admin_manage.php
@@ -3,12 +3,11 @@
  * Kanban Users (kanban-users)
  * @var $this UserController 
  * @var $model KanbanUsers 
- * version: 0.0.1
  *
  * @author Putra Sudaryanto <putra@sudaryanto.id>
+ * @contact (+62)856-299-4114
  * @copyright Copyright (c) 2013 Ommu Platform (opensource.ommu.co)
  * @link https://github.com/ommu/ommu-kanban-task
- * @contact (+62)856-299-4114
  *
  */
 
